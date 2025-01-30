ebe305141b50311b8c2aa2a4e2d76888a39eed31
lkhalifa <lkhalifa@42.fr>
2025-01-30 15:36:31 +0100

    not ock - ps in wp script

--- srcs/requirements/wordpress/tools/wordpress_install.sh
+++ srcs/requirements/wordpress/tools/wordpress_install.sh
@@ -6,16 +6,16 @@ WP_DIR="/var/www/html"
 
 # Install wordpress
 curl -LO https://wordpress.org/latest.tar.gz
-tar -xvzf latest.tar.gz
+tar -xvzf latest.tar.gz $WP_DIR/
 chown -R www-data:www-data $WP_DIR/
 chmod -R 755 $WP_DIR/
 rm latest.tar.gz
 
 # Set up the wp-config.php file for WordPress
 cp $WP_DIR/wp-config-sample.php $WP_DIR/wp-config.php
-sed -i "s/database_name_here/$WP_DB_NAME/" $WP_DIR/wp-config.php
-sed -i "s/username_here/$WP_DB_USER/" $WP_DIR/wp-config.php
-sed -i "s/password_here/$WP_DB_PASSWORD/" $WP_DIR/wp-config.php
+sed -i "s/database_name_here/$MYSQL_DATABASE/" $WP_DIR/wp-config.php
+sed -i "s/username_here/$MYSQL_USER/" $WP_DIR/wp-config.php
+sed -i "s/password_here/$MYSQL_PASSWORD/" $WP_DIR/wp-config.php
 
 SALTS=$(curl -s https://api.wordpress.org/secret-key/1.1/salt/)
 sed -i "/# Authentication Unique Keys and Salts/ r /dev/stdin" $WP_DIR/wp-config.php <<< "$SALTS"
