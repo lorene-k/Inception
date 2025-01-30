89ac8bdcfe0d4dbe639bd4deb9d393aa1b4cbe10
lkhalifa <lkhalifa@42.fr>
2025-01-30 18:39:34 +0100

    wp not ok - fix conf files

--- srcs/requirements/wordpress/tools/wordpress_install.sh
+++ srcs/requirements/wordpress/tools/wordpress_install.sh
@@ -2,22 +2,20 @@
 
 set -e
 
-WP_DIR="/var/www/html"
-
 # Install wordpress
 curl -LO https://wordpress.org/latest.tar.gz
-tar -xvzf latest.tar.gz $WP_DIR/
-chown -R www-data:www-data $WP_DIR/
-chmod -R 755 $WP_DIR/
+tar -xvzf latest.tar.gz
+chown -R www-data:www-data wordpress/
+chmod -R 755 wordpress/
 rm latest.tar.gz
 
 # Set up the wp-config.php file for WordPress
-cp $WP_DIR/wp-config-sample.php $WP_DIR/wp-config.php
-sed -i "s/database_name_here/$MYSQL_DATABASE/" $WP_DIR/wp-config.php
-sed -i "s/username_here/$MYSQL_USER/" $WP_DIR/wp-config.php
-sed -i "s/password_here/$MYSQL_PASSWORD/" $WP_DIR/wp-config.php
+cp wordpress/wp-config-sample.php wordpress/wp-config.php
+sed -i "s/database_name_here/$MYSQL_DATABASE/" wordpress/wp-config.php
+sed -i "s/username_here/$MYSQL_USER/" wordpress/wp-config.php
+sed -i "s/password_here/$MYSQL_PASSWORD/" wordpress/wp-config.php
 
-SALTS=$(curl -s https://api.wordpress.org/secret-key/1.1/salt/)
-sed -i "/# Authentication Unique Keys and Salts/ r /dev/stdin" $WP_DIR/wp-config.php <<< "$SALTS"
+# SALTS=$(curl -s https://api.wordpress.org/secret-key/1.1/salt/)
+# sed -i "/# Authentication Unique Keys and Salts/ r /dev/stdin" ./wp-config.php <<< "$SALTS"
 
 exec php-fpm7.4 -F
