1e445d8d504fa1dca32a653b0a57767691b0cb41
lo2999 <lorenejulie@hotmail.com>
2025-02-02 10:41:56 +0000

    wordpress ok git add srcs

--- srcs/requirements/wordpress/tools/wordpress_install.sh
+++ srcs/requirements/wordpress/tools/wordpress_install.sh
@@ -2,23 +2,24 @@
 
 set -e
 
-if [ -f ./wordpress/wp-config.php ]
+if [ -f wp-config.php ]
 then
 	echo "Wordpress already configured"
 else
     # --- Install wordpress ---
     curl -LO https://wordpress.org/latest.tar.gz
     tar -xvzf latest.tar.gz
-    chown -R www-data:www-data wordpress/
-    chmod -R 755 wordpress/
+    chown -R www-data:www-data ./
+    chmod -R 755 ./
+    mv wordpress/* ./
     rm latest.tar.gz
 
     # --- Setup wp-config.php ---
-    cp wordpress/wp-config-sample.php wordpress/wp-config.php
-    sed -i "s/database_name_here/$MYSQL_DATABASE/" wordpress/wp-config.php
-    sed -i "s/localhost/$MYSQL_HOSTNAME/g" wordpress/wp-config.php
-    sed -i "s/username_here/$MYSQL_USER/" wordpress/wp-config.php
-    sed -i "s/password_here/$MYSQL_PASSWORD/" wordpress/wp-config.php
+    cp wp-config-sample.php wp-config.php
+    sed -i "s/database_name_here/${MYSQL_DATABASE}/" wp-config.php
+    sed -i "s/localhost/${MYSQL_HOSTNAME}/" wp-config.php
+    sed -i "s/username_here/${MYSQL_USER}/" wp-config.php
+    sed -i "s/password_here/${MYSQL_PASSWORD}/" wp-config.php
 fi
 
 exec php-fpm7.4 -F
