2afcb8172ea01dc35e7d3b251fa9b071376ee6e5
lo2999 <lorenejulie@hotmail.com>
2025-02-09 23:33:41 +0000

    wp automatic install configured - 2 wp user OK (+ 1 sql user)

--- srcs/requirements/mariadb/tools/mariadb_init.sh
+++ srcs/requirements/mariadb/tools/mariadb_init.sh
@@ -21,11 +21,10 @@ cat << EOF > $tmp
 	ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
 
 	CREATE DATABASE $MYSQL_DATABASE CHARACTER SET utf8 COLLATE utf8_general_ci;
-	CREATE USER '$MYSQL_USER'@'%' IDENTIFIED by '$MYSQL_PASSWORD';
+	CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED by '$MYSQL_PASSWORD';
 	GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
-
 	FLUSH PRIVILEGES;
 EOF
 mysqld --user=mysql --bootstrap < $tmp
 
-exec mysqld --console
+exec mysqld --user=mysql
--- srcs/requirements/wordpress/tools/wordpress_install.sh
+++ srcs/requirements/wordpress/tools/wordpress_install.sh
@@ -7,20 +7,39 @@ then
 	echo "Wordpress already configured"
 else
     sleep 2
-    # --- Install wordpress ---
-    curl -LO https://wordpress.org/latest.tar.gz
-    tar -xvzf latest.tar.gz
+
+    # --- Set permissions ---
     chown -R www-data:www-data ./
     chmod -R 755 ./
+
+    # --- Download and extract WordPress ---
+    curl -LO https://wordpress.org/latest.tar.gz
+    tar -xvzf latest.tar.gz
     mv wordpress/* ./
-    rm latest.tar.gz
+    rm -rf wordpress latest.tar.gz
 
     # --- Setup wp-config.php ---
-    cp wp-config-sample.php wp-config.php
-    sed -i "s/database_name_here/${MYSQL_DATABASE}/" wp-config.php
-    sed -i "s/localhost/${MYSQL_HOSTNAME}/" wp-config.php
-    sed -i "s/username_here/${MYSQL_USER}/" wp-config.php
-    sed -i "s/password_here/${MYSQL_PASSWORD}/" wp-config.php
+    wp config create \
+        --dbname="$MYSQL_DATABASE" \
+        --dbuser="$MYSQL_USER" \
+        --dbpass="$MYSQL_PASSWORD" \
+        --dbhost="$MYSQL_HOSTNAME" \
+        --allow-root
+
+    # --- Install WordPress ---
+    wp core install \
+        --url="$DOMAIN_NAME" \
+        --title="42_Inception Wordpress" \
+        --admin_user="$WP_ADM_USER" \
+        --admin_password="$WP_ADM_PW" \
+        --admin_email="wpboss@test.com" \
+        --allow-root
+
+    # --- Create Editor User ---
+    wp user create "$WP_USER" "randomuser@test.com" \
+        --role=editor \
+        --user_pass="$WP_PW" \
+        --allow-root
 fi
 
 exec php-fpm7.4 -F
